245a9d477a34ad73200004ad2a458387f13611b3
Joytis <cchambers@avalanwireless.com>
2017-01-21 23:37:35 -0600

    Adding sand scuffs and crystal tinks

---
+++ Assets/Scripts/OnClickSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickSound : MonoBehaviour {
+
+	public AudioClip clip;
+	AudioSource source;
+
+	// Use this for initialization
+	void Awake () {
+		source = gameObject.AddComponent<AudioSource>();
+		source.clip = clip;
+	}
+	
+	void OnMouseDown() {
+		source.Play();
+	}
+}
